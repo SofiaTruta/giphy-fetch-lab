9fb20266cd8dc9f8f078c15032d0ad081524f16b
weezleb <basil.housden@yahoo.com>
2023-10-23 11:41:50 +0100

    searchbar.jsx

--- giphy-fetch-lab/src/App.js
+++ giphy-fetch-lab/src/App.js
@@ -3,19 +3,10 @@ import SearchBar from './components/SearchBar';
 import GifList from './components/GifList';
 
 
-<<<<<<< HEAD
 const App = () => {
   const [gifs, setGifs] = useState([]);
 
-  const doThaSearchInit = async (searchTerm) => {
-    const apiKey = process.env.REACT_APP_GIPHY_API_KEY;
-    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}`;
 
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    setGifs(data.data);
-  };
-=======
   async function onSearch(searchTerm) {
     try {
       const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${process.env.REACT_APP_GIPHY_API_KEY}&limit=8`)
@@ -26,12 +17,12 @@ const App = () => {
       console.error(err)
     }
   }
->>>>>>> main
+
 
   return (
     <div>
       <h1>Search for GIFs</h1>
-      <SearchBar onSearch={doThaSearchInit} />
+      <SearchBar onSearch={onSearch} />
       <GifList gifs={gifs} />
     </div>
   );
